9052950e618565c3d452631c3e8e60b653595c96
Heinz1869 <butanikuteikoku@gmail.com>
2026-06-14 08:37:23 +0900

    update: README, architecture diagram and project files

--- provider.tf
+++ provider.tf
@@ -2,3 +2,12 @@
 provider "aws" {
   region = "ap-northeast-1" # 東京リージョン
 }
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
