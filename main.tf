333415531679d27f0c8c13f1f8d5d36388765cf8
Heinz1869 <butanikuteikoku@gmail.com>
2026-06-14 06:17:58 +0900

    Initial commit with clean structure

---
+++ main.tf
@@ -0,0 +1,8 @@
+# VPCの定義
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "bastion-demo-vpc"
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,4 @@
+# AWSプロバイダーの設定
+provider "aws" {
+  region = "ap-northeast-1" # 東京リージョン
+}
